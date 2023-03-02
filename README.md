f9296032c40b35f7b750a48bd1df264f777522ac
Jinus Mk <jinusmk@Jinuss-MacBook-Pro.local>
2023-03-02 12:37:17 +0530

    feat: init commit

---
+++ README.md
@@ -0,0 +1,64 @@
+# Portfolio-builder
+
+A clean and simple portfolio template built using React, if want a template built with HTML, CSS and JS then check out
+
+## Preview
+
+[![Imgur](https://imgur.com/FwDMNEM.gif)](https://jinusmk.github.io/portfolio-builder)
+
+[See Live](https://jinusmk.github.io/portfolio-builder)
+
+## Instructions
+
+### Setup
+
+```shell
+git clone https://github.com/jinusmk/portfolio-builder-reactjs
+cd portfolio-builder-reactjs
+```
+
+If you use [nvm](https://github.com/nvm-sh/nvm) or [fnm](https://github.com/Schniz/fnm) (optional) -
+
+```shell
+nvm install
+nvm use
+```
+
+Or -
+
+```shell
+fnm install
+fnm use
+```
+
+Install and run -
+
+```shell
+yarn
+yarn start
+```
+
+### How to use
+
+- Open `public/index.html` and replace -
+
+`<title>John Smith</title>` with `<title>Your Name</title>`.
+
+- Open `src/portfolio.js` and make changes.
+
+### Deployment
+
+- Go to `package.json` file and replace -
+
+`"homepage": "https://rajshekhar26.github.io/cleanfolio"`
+
+with `"homepage": "https://yourusername.github.io"`.
+
+- Push the changes.
+
+- Build and deploy -
+
+```shell
+yarn build
+yarn deploy
+```
---
+++ src/portfolio.js
@@ -0,0 +1,64 @@
+const header = {
+  homepage: "https://jinusmk.github.io/portfolio-builder",
+  title: "JS",
+};
+
+const about = {
+  name: "John Smith",
+  role: "Front End Engineer",
+  description:
+    "Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.",
+  resume: "https://example.com",
+  social: {
+    linkedin: "https://linkedin.com",
+    github: "https://github.com",
+  },
+};
+
+const projects = [
+  {
+    name: "Project 1",
+    description:
+      "Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam",
+    stack: ["SASS", "TypeScript", "React"],
+    sourceCode: "https://github.com",
+    livePreview: "https://github.com",
+  },
+  {
+    name: "Project 2",
+    description:
+      "Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam",
+    stack: ["SASS", "TypeScript", "React"],
+    sourceCode: "https://github.com",
+    livePreview: "https://github.com",
+  },
+  {
+    name: "Project 3",
+    description:
+      "Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam",
+    stack: ["SASS", "TypeScript", "React"],
+    sourceCode: "https://github.com",
+    livePreview: "https://github.com",
+  },
+];
+
+const skills = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "TypeScript",
+  "React",
+  "Redux",
+  "SASS",
+  "Material UI",
+  "Git",
+  "CI/CD",
+  "Jest",
+  "Enzyme",
+];
+
+const contact = {
+  email: "johnsmith@mail.com",
+};
+
+export { header, about, projects, skills, contact };
